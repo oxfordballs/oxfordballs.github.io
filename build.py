193f1fe92d7d6d52db0ea611db1dc93f77f8379d
oxfordballs <15522328+oxfordballs@users.noreply.github.com>
2021-11-06 13:36:17 -0400

    Create build.py

---
+++ build.py
@@ -0,0 +1,11 @@
+import os
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+print(os.listdir())
