aaaf3dd5fea4f260b2a7b1ac0d4fe58f85403a51
oxfordballs <15522328+oxfordballs@users.noreply.github.com>
2021-11-06 14:39:19 -0400

    Update build.py

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
 SPREADSHEET_ID = "1Xxs0KaEaD3XgQdrlNxbg25qwG2YvoBdoRJsIdDZIjvw"
-SAMPLE_RANGE_NAME = 'Sheet1!A1:2'
+SHEET_RANGE = 'Sheet1!A2:H100'
 
 build_dir = "build"
 os.mkdir(build_dir)
@@ -21,13 +21,11 @@ service = build('sheets', 'v4', credentials=creds)
 
 # Call the Sheets API
 sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SHEET_RANGE).execute()
 values = result.get('values', [])
 
-if not values:
-    print('No data found.')
-else:
-    print('Name, Major:')
-    for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
-        print('%s, %s' % (row[0], row[4]))
+for row in values:
+    event_name, date, theme, dress_code, prices, ticket_status, event_url, ticket_url = row
+    if event_name == "":
+      break
+    print(event_name, date, theme, dress_code, prices, ticket_status, event_url, ticket_url)
